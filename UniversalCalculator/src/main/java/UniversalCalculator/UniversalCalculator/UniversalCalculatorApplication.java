95246231724d40a2b5bc5669c5bc954f8febee7b
Maximus <fedosenkomaksim946@gmail.com>
2023-12-03 17:25:48 +0200

    Basic

---
+++ UniversalCalculator/src/main/java/UniversalCalculator/UniversalCalculator/UniversalCalculatorApplication.java
@@ -0,0 +1,13 @@
+package UniversalCalculator.UniversalCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalCalculatorApplication.class, args);
+	}
+
+}
---
+++ UniversalCalculator/src/test/java/UniversalCalculator/UniversalCalculator/UniversalCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package UniversalCalculator.UniversalCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
